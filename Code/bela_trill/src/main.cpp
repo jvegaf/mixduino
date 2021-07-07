ec5fe956c06cf779d02131085c3ee5bf9f2cb11c
Jose Vega <josevega234@gmail.com>
2021-07-07 11:15:45 +0200

    trill pruebas

---
+++ Code/bela_trill/src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <Trill.h>
+
+Trill trillSensor;
+boolean touchActive = false;
+
+void setup() {
+  // Initialise serial and touch sensor
+  Serial.begin(115200);
+  int ret = trillSensor.setup(Trill::TRILL_BAR);
+  if(ret != 0) {
+    Serial.println("failed to initialise trillSensor");
+    Serial.print("Error code: ");
+    Serial.println(ret);
+  }
+}
+
+void loop() {
+  // Read 20 times per second
+  delay(50);
+  trillSensor.read();
+
+  if(trillSensor.getNumTouches() > 0) {
+    for(int i = 0; i < trillSensor.getNumTouches(); i++) {
+        Serial.print(trillSensor.touchLocation(i));
+        Serial.print(" ");
+        Serial.print(trillSensor.touchSize(i));
+        Serial.print(" ");
+    }
+    Serial.println("");
+    touchActive = true;
+  }
+  else if(touchActive) {
+    // Print a single line when touch goes off
+    Serial.println("0 0");
+    touchActive = false;
+  }
+}

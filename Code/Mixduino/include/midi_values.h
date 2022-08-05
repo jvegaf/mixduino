e2289268d49f09e44bbbdcf297108c2c557cacdc
Jose Vega <josevega234@gmail.com>
2022-08-06 01:52:21 +0200

    midi values
    Signed-off-by: Jose Vega <josevega234@gmail.com>

---
+++ Code/Mixduino/include/midi_values.h
@@ -0,0 +1,84 @@
+#ifndef MD_MIDI_VALUES_H
+#define MD_MIDI_VALUES_H
+
+#include <Arduino.h>
+
+const uint8_t km_C_0  = 12;
+const uint8_t km_Cs_0 = 13;
+const uint8_t km_D_0  = 14;
+const uint8_t km_Ds_0 = 15;
+const uint8_t km_E_0  = 16;
+const uint8_t km_F_0  = 17;
+const uint8_t km_Fs_0 = 18;
+const uint8_t km_G_0  = 19;
+const uint8_t km_Gs_0 = 20;
+const uint8_t km_A_0  = 21;
+const uint8_t km_As_0 = 22;
+const uint8_t km_B_0  = 23;
+
+const uint8_t km_C_1  = 24;
+const uint8_t km_Cs_1 = 25;
+const uint8_t km_D_1  = 26;
+const uint8_t km_Ds_1 = 27;
+const uint8_t km_E_1  = 28;
+const uint8_t km_F_1  = 29;
+const uint8_t km_Fs_1 = 30;
+const uint8_t km_G_1  = 31;
+const uint8_t km_Gs_1 = 32;
+const uint8_t km_A_1  = 33;
+const uint8_t km_As_1 = 34;
+const uint8_t km_B_1  = 35;
+
+const uint8_t km_C_2  = 36;
+const uint8_t km_Cs_2 = 37;
+const uint8_t km_D_2  = 38;
+const uint8_t km_Ds_2 = 39;
+const uint8_t km_E_2  = 40;
+const uint8_t km_F_2  = 41;
+const uint8_t km_Fs_2 = 42;
+const uint8_t km_G_2  = 43;
+const uint8_t km_Gs_2 = 44;
+const uint8_t km_A_2  = 45;
+const uint8_t km_As_2 = 46;
+const uint8_t km_B_2  = 47;
+
+const uint8_t km_C_3  = 48;
+const uint8_t km_Cs_3 = 49;
+const uint8_t km_D_3  = 50;
+const uint8_t km_Ds_3 = 51;
+const uint8_t km_E_3  = 52;
+const uint8_t km_F_3  = 53;
+const uint8_t km_Fs_3 = 54;
+const uint8_t km_G_3  = 55;
+const uint8_t km_Gs_3 = 56;
+const uint8_t km_A_3  = 57;
+const uint8_t km_As_3 = 58;
+const uint8_t km_B_3  = 59;
+
+const uint8_t km_C_4  = 60;
+const uint8_t km_Cs_4 = 61;
+const uint8_t km_D_4  = 62;
+const uint8_t km_Ds_4 = 63;
+const uint8_t km_E_4  = 64;
+const uint8_t km_F_4  = 65;
+const uint8_t km_Fs_4 = 66;
+const uint8_t km_G_4  = 67;
+const uint8_t km_Gs_4 = 68;
+const uint8_t km_A_4  = 69;
+const uint8_t km_As_4 = 70;
+const uint8_t km_B_4  = 71;
+
+const uint8_t km_C_5  = 72;
+const uint8_t km_Cs_5 = 73;
+const uint8_t km_D_5  = 74;
+const uint8_t km_Ds_5 = 75;
+const uint8_t km_E_5  = 76;
+const uint8_t km_F_5  = 77;
+const uint8_t km_Fs_5 = 78;
+const uint8_t km_G_5  = 79;
+const uint8_t km_Gs_5 = 80;
+const uint8_t km_A_5  = 81;
+const uint8_t km_As_5 = 82;
+const uint8_t km_B_5  = 83;
+
+#endif // MD_MIDI_VALUES_H
